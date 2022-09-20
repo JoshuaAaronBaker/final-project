733bb73925fc9183e0e72ed37ddeaac2134745ad
Joshua Baker <jbaker264@yahoo.com>
2022-09-20 22:15:00 +0000

    User can now see their last 20 recently played songs, their own playlists, and some of spotify's featured playlists

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+import './home.css';
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      data: null
+      userPlaylists: {},
+      featuredPlaylists: {},
+      recentlyPlayed: {},
+      profile: {}
     };
   }
 
@@ -16,6 +20,24 @@ export default class Home extends React.Component {
         const playlists = res.data;
         this.setState({ isLoading: false, userPlaylists: playlists });
       });
+
+    axios.get('/browse/featured-playlists')
+      .then(res => {
+        const featuredPlaylists = res.data;
+        this.setState({ featuredPlaylists });
+      });
+
+    axios.get('/me/player/recently-played')
+      .then(res => {
+        const recentlyPlayed = res.data;
+        this.setState({ recentlyPlayed });
+      });
+
+    axios.get('/me')
+      .then(res => {
+        const profile = res.data;
+        this.setState({ profile });
+      });
   }
 
   render() {
@@ -23,11 +45,64 @@ export default class Home extends React.Component {
       return null;
     }
     return (
-      <ul>
-        {
-          this.state.userPlaylists.items.map(playlist => <li key={playlist.id}><img src={playlist.images[0].url} alt="playlist image" /></li>)
-        }
-      </ul>
+      <div className="container overflow backgrond">
+        <div className="row">
+          <h2>Good Afternoon</h2>
+        </div>
+        <div className="row slider">
+          {this.state.recentlyPlayed.items.map(song =>
+            <div key={song.id} className='col-third margin10 align-center'>
+              <div className='col-three-fourth'>
+                <img className='coverart' src={song.track.album.images[0].url} alt="" />
+              </div>
+              <div className='col-fourth text-align-center'>
+                <p>{song.track.name}</p>
+              </div>
+            </div>)}
+        </div>
+        <div className="row">
+          <h3>{`${this.state.profile.display_name}'s`} Playlists</h3>
+        </div>
+        <div className="row slider">
+            {this.state.userPlaylists.items.map(playlist =>
+              <div key={playlist.id} className='col-third margin10 align-center'>
+                <div className='col-three-fourth'>
+                  <img className='coverart' src={playlist.images[0].url} alt="" />
+                </div>
+                <div className='col-fourth text-align-center'>
+                  <p>{playlist.name}</p>
+                </div>
+              </div>)}
+        </div>
+        <div className="row">
+          <h3>Featured Playlists</h3>
+        </div>
+        <div className="row slider">
+          {this.state.featuredPlaylists.playlists.items.map(playlist =>
+            <div key={playlist.id} className='col-third margin10 align-center'>
+              <div className='col-three-fourth'>
+                <img className='coverart' src={playlist.images[0].url} alt="" />
+              </div>
+              <div className='col-fourth text-align-center'>
+                <p>{playlist.name}</p>
+              </div>
+            </div>)}
+        </div>
+        {/* <div className="row">
+          <h3>Featured Playlists</h3>
+        </div>
+        <div className="row slider">
+          {this.state.featuredPlaylists.playlists.items.map(playlist =>
+            <div key={playlist.id} className='col-third margin10 align-center'>
+              <div className='col-three-fourth'>
+                <img className='coverart' src={playlist.images[0].url} alt="" />
+              </div>
+              <div className='col-fourth text-align-center'>
+                <p>{playlist.name}</p>
+              </div>
+            </div>)}
+        </div> */}
+      </div>
     );
   }
 }
